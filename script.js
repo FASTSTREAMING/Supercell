0c65780ab55b14f9a7e2ae5030f89fa77aae5540
FASTSTREAMING <133608111+FASTSTREAMING@users.noreply.github.com>
2024-06-10 23:19:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Previene el envío normal del formulario
+
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var telefono = document.getElementById('telefono').value;
+
+    if (!document.getElementById('terms').checked) {
+        alert('Por favor, lee y acepta los términos y condiciones para continuar.');
+        return false;
+    }
+    var mensaje = encodeURIComponent("Yo, " + nombre + " " + apellido + ", con número de teléfono " + telefono + ", confirmo que he leído y acepto los términos y condiciones. Autorizo el uso de mis datos conforme a la política establecida.");
+    var whatsappUrl = "https://wa.me/59176803090?text=" + mensaje;
+
+    var thankYouPopup = document.getElementById('thankYouPopup');
+    thankYouPopup.style.display = "block";
+
+    var progressBar = document.getElementById('progressBar');
+    progressBar.style.width = "0%";
+    setTimeout(function() {
+        progressBar.style.width = "100%";
+    }, 10);
+
+    setTimeout(function() {
+        window.location.href = whatsappUrl;
+    }, 3000);
+});
+
+// Manejo de la ventana modal para términos y condiciones
+var termsModal = document.getElementById('termsModal');
+var termsBtn = document.getElementById('termsLink');
+var termsSpan = termsModal.getElementsByClassName("close")[0];
+
+termsBtn.onclick = function() {
+    termsModal.style.display = "block";
+}
+
+termsSpan.onclick = function() {
+    termsModal.style.display = "none";
+}
+// Manejo de la ventana emergente de agradecimiento
+var thankYouPopup = document.getElementById('thankYouPopup');
+var thankYouSpan = thankYouPopup.getElementsByClassName("close")[0];
+
+thankYouSpan.onclick = function() {
+    thankYouPopup.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == termsModal) {
+        termsModal.style.display = "none";
+    }
+    if (event.target == thankYouPopup) {
+        thankYouPopup.style.display = "none";
+    }
+}
